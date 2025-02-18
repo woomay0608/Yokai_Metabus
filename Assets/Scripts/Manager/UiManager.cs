2d81e6d2353591c438d39e42f1d8c563d79625a2
woomay0608 <woohuck0608@naver.com>
2025-02-18 14:48:14 +0900

    [Feat] 동료와의 대화 구현

--- Assets/Scripts/Manager/UiManager.cs
+++ Assets/Scripts/Manager/UiManager.cs
@@ -1,8 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-   
+    [SerializeField] private TextMeshProUGUI Talk;
+    [SerializeField] private Image image;
+    [SerializeField] private GameObject GameObject;
+
+    private UiManager uiManager;
+
+
+
+    private void Awake()
+    {
+        if(GameObject ==  null)
+        {
+            GameObject = GameObject.Find("TalkObject");
+        }
+    }
+
+    public  void StartCorutine(List<string> strings)
+    {
+        StartCoroutine(showtext(strings));
+    }
+    public void SetAct()
+    {
+        GameObject.SetActive(true);
+    }
+    public void SetFalse()
+    {
+        GameObject.SetActive(false);
+    }
+
+    public void SetIma(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    private IEnumerator showtext(List<string> list)
+    {
+
+        for (int i = 0; i < list.Count; i++)  
+        {
+            Talk.text = list[i];
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
 }
--- Assets/Scripts/friends.cs
+++ Assets/Scripts/friends.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 public class friends : MonoBehaviour
 {
 
-   
+
+    [SerializeField] private int FriendId;
     [SerializeField] private TMP_Text talkText;
+    [SerializeField] private Sprite Face;
+
+    List<string> talk = new List<string>();
+
+
+    UiManager uiManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        uiManager = FindAnyObjectByType<UiManager>();
     }
     //
 
@@ -20,18 +27,41 @@ public class friends : MonoBehaviour
     //Collistion/ Trigger
     //enter , stay, Exit
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         talkText.gameObject.SetActive(true);
         Debug.Log("¾È³ç");
+
+        SetText();
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log("nurum");
+            uiManager.SetAct();
+            uiManager.SetIma(Face);
+            uiManager.StartCorutine(talk);
+
+        }
     }
+ 
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         talkText.gameObject.SetActive(false);
+        uiManager.SetFalse();
         Debug.Log("¾È³ç¤¸¤¸");
     }
 
- 
+
+    private void SetText()
+    {
+        if (FriendId ==1)
+        {
+            talk.Add("Kainin nim");
+            talk.Add("han panheayo");
+        }
+    }
+
+
  
 }
