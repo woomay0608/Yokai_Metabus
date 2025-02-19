76dda950ea9b942754fb88914726c10afe847d6f
woomay0608 <woohuck0608@naver.com>
2025-02-19 15:19:03 +0900

    {modify] 리더보드 수정 (해결X)

---
+++ Assets/Scripts/ReaderBorad.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class ReaderBorad : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private TextMeshPro Flapy;
+    public List<int> FlapyList = new List<int>();
+    void Start()
+    {
+        ReaderBoardSet();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReaderBoardSet() //ReaderBoard에다가 넣자
+    {
+        StringBuilder sb = new StringBuilder();
+        int number = 1;
+        sb.Append("Flappy\n");
+
+        if (FlapyList != null)
+        {
+            foreach (int i in FlapyList)
+            {
+                sb.Append($"{number}. {i.ToString()}\n");
+                number++;
+
+            }
+        }
+        else
+        {
+            Debug.Log("FlappyList가 널임");
+        }
+
+        Debug.Log("리더보드 업데이트됨: " + sb.ToString());
+
+        if (Flapy != null)
+            Flapy.text = sb.ToString();
+
+
+    }
+}
